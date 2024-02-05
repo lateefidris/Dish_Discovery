34545232f5b9f6df16efd2fb8230dbaf31e896a5
Lateef Idris <150227412+lateefidris@users.noreply.github.com>
2024-02-05 01:14:56 +0000

    App Works

--- app.rb
+++ app.rb
@@ -1,9 +1,37 @@
 require "sinatra"
 require "sinatra/reloader"
+require "http"
+require "json"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+
+  erb(:homepage)
+end
+
+
+post("/result") do
+  key = ENV.fetch("FOOD_KEY")
+  random_recipe_url = "https://api.spoonacular.com/recipes/random?number=1&apiKey=#{key}"
+
+  raw_response = HTTP.get(random_recipe_url).to_s
+  parsed_response = JSON.parse(raw_response)
+  recipe = parsed_response.fetch("recipes")
+  data = recipe[0]
+
+  #data
+  @title = data.fetch("title")
+  @prep_time = data.fetch("readyInMinutes").to_s
+  @servings = data.fetch("servings").to_s
+  @image = data.fetch("image")
+  @summary = data.fetch("summary")
+  @instructions = data.fetch("instructions")
+
+  #instructions
+  deep_instructions = data.fetch("analyzedInstructions")[0]
+  @steps = deep_instructions.fetch("steps")
+
+  #ingredients
+  @ingredients = data.fetch("extendedIngredients")
+
+  erb(:result)
 end
